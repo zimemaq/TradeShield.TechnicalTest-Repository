7c4630533f432e0b5a5a945a7a86aadba478a121
Qhamiso Zimema <vqzimema@outlook.com>
2024-07-21 18:00:44 +0200

    Introduced nested loops to get the max depth

--- TS.Domain/Services/DeepestPit.cs
+++ TS.Domain/Services/DeepestPit.cs
@@ -11,32 +11,45 @@ namespace TS.Domain.Services
         public int GetPitDepth(int[] points)
         {
             int arrayLength = points.Length;
-
             int maximumDepth = -1;
-            int P = 0;
-            int Q = -1;
+
+            //int P = 0;
+            //int Q = -1;
             int R = -1;
 
-            for (int i = 1; i < arrayLength; i++)
+            // Try solution with nested loops. This might introduce performance issues on large arrays
+            for(int P = 0; P < arrayLength - 2; P++)
             {
-                if(Q < 0 && points[i] < points[i - 1])
+                for(int Q = P + 1; Q < arrayLength - 1; Q++)
                 {
-                    Q = i - 1;
+                    if (R >= 0 && points[P] > points[Q - 1])
+                    {
+                        maximumDepth = Math.Max(maximumDepth, Math.Min(points[P] - points[Q], points[R] - points[Q]));
+                        R = -1;
+                    }
                 }
-
-                if(Q >= 0 && (R < 0 || points[i] < points[i - 1]))
-                {
-                    R = 1;
-                }
-
-                if(R >= 0 && points[i] > points[i - 1])
-                {
-                    maximumDepth = Math.Max(maximumDepth, Math.Min(points[P] - points[Q], points[R] - points[Q]));
-                    P = i - 1;
-                    Q = -1;
-                    R = -1;
-                }                
             }
+
+            //for (int i = 1; i < arrayLength; i++)
+            //{
+            //    if(Q < 0 && points[i] < points[i - 1])
+            //    {
+            //        Q = i - 1;
+            //    }
+
+            //    if(Q >= 0 && (R < 0 || points[i] < points[i - 1]))
+            //    {
+            //        R = 1;
+            //    }
+
+            //    if(R >= 0 && points[i] > points[i - 1])
+            //    {
+            //        maximumDepth = Math.Max(maximumDepth, Math.Min(points[P] - points[Q], points[R] - points[Q]));
+            //        P = i - 1;
+            //        Q = -1;
+            //        R = -1;
+            //    }                
+            //}
 
             return maximumDepth;
         }
